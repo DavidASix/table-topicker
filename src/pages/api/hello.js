955c2724b095a5a4a23b02f215889b8c793d27e9
David Anderson <davidandersonsix@gmail.com>
2024-04-02 22:16:29 -0400

    Removed edge runtime

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { connectToDatabase } from "@/database";
 
-export const runtime = 'edge';
 
 export default async function handler(req, res) {
   const { db } = await connectToDatabase();
