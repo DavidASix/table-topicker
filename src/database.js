1465fedf233971654f211493ff322b2fa3677f37
David Anderson <davidandersonsix@gmail.com>
2024-04-03 16:18:57 -0400

    Added multi caching for promises

--- src/database.js
+++ src/database.js
@@ -11,30 +11,33 @@ const databases = {
  */
 let cached = global.mongo;
 if (!cached) {
-  cached = global.mongo = { connections: new Map(), promise: null };
+  cached = global.mongo = { connections: new Map(), promises: new Map() };
 }
 
 export async function connectToDatabase(selectedDb) {
   const dbName = databases[selectedDb];
+  console.log(`Called DB for db ${selectedDb} : ${dbName}`)
   if (!dbName) {
     throw new Error('Database does not exist');
   }
 
   if (cached.connections.has(selectedDb)) {
+    console.log('using cached DB')
     return cached.connections.get(selectedDb);
   }
 
-  if (!cached.promise) {
+  if (!cached.promises.has(selectedDb)) {
     const opts = { useUnifiedTopology : true }
 
-    cached.promise = MongoClient.connect(MONGODB_URI, opts).then((client) => {
+    cached.promises.set(selectedDb, MongoClient.connect(MONGODB_URI, opts).then((client) => {
       return {
         client,
         db: client.db(dbName),
       }
-    })
+    }));
   }
-  const conn = await cached.promise;
+
+  const conn = await cached.promises[selectedDb];
   cached.connections.set(selectedDb, conn);
   return conn
 }
