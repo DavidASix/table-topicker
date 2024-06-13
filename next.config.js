d19f134d775647b00e539c89b5c3fa9b94c7dfaf
David Anderson <davidandersonsix@gmail.com>
2024-06-13 17:11:54 -0400

    Installed SVG Webpack library and updated next config to handle SVG Imports

--- next.config.js
+++ next.config.js
@@ -2,11 +2,44 @@
 
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+const webpackConfig = {
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    )
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+    )
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i
+    return config
+  }
+}
+
+const baseConfig = {...webpackConfig, ...nextConfig};
+
+// Handle PWA switch and return config
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      reactStrictMode: true,
-    };
+    return baseConfig;
   }
   // PWA in production
   const withPWA = require("next-pwa")({
@@ -14,7 +47,5 @@ module.exports = (phase, { defaultConfig }) => {
     register: true,
     skipWaiting: true,
   });
-  return withPWA({
-    reactStrictMode: true,
-  })
+  return withPWA(baseConfig)
 };
