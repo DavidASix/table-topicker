15794c6307e635677aceb71f2be7cfb9dfe666b2
davidasix <david@davidasix.com>
2025-11-09 18:54:06 -0500

    Add base questions and store source

---
+++ src/scripts/base-questions.ts
@@ -0,0 +1,536 @@
+export const baseQuestions = [
+  {
+    theme: "Growth & Change",
+    question: "When was the last time you tried something new?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Self & Identity",
+    question: "Who do you sometimes compare yourself to?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Values & Morality",
+    question: "What's the most sensible thing you've ever heard someone say?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question: "What gets you excited about life?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Growth & Change",
+    question: "What life lesson did you learn the hard way?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Time & Legacy",
+    question: "What do you wish you spent more time doing five years ago?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Self & Identity",
+    question: "Do you ask enough questions or do you settle for what you know?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Relationships & Connection",
+    question: "Who do you love and what are you doing about it?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Values & Morality",
+    question: "What's a belief that you hold with which many people disagree?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Growth & Change",
+    question: "What can you do today that you were not capable of a year ago?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Self & Identity",
+    question: "Do you think crying is a sign of weakness or strength?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Self & Identity",
+    question:
+      "What would you do differently if you knew nobody would judge you?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question: "Do you celebrate the things you do have?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question: "What is the difference between living and existing?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Growth & Change",
+    question: "If not now, then when?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Time & Legacy",
+    question: "Have you done anything lately worth remembering?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question: "What does your joy look like today?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Values & Morality",
+    question: "Is it possible to lie without saying a word?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Self & Identity",
+    question:
+      "If you had a friend who spoke to you in the same way that you sometimes speak to yourself, how long would you allow this person to be your friend?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Self & Identity",
+    question: "Which activities make you lose track of time?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Self & Identity",
+    question: "If you had to teach something, what would you teach?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Time & Legacy",
+    question:
+      "What would you regret not fully doing, being or having in your life?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Growth & Change",
+    question: "Are you holding onto something that you need to let go of?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Time & Legacy",
+    question: "When you are 80-years-old, what will matter to you the most?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Values & Morality",
+    question:
+      "When is it time to stop calculating risk and rewards and just do what you know is right?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Self & Identity",
+    question: "How old would you be if you didn't know how old you are?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Values & Morality",
+    question: "Would you break the law to save a loved one?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question: "What makes you smile?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Values & Morality",
+    question:
+      "When it's all said and done, will you have said more than you've done?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Relationships & Connection",
+    question:
+      "If you had the opportunity to get a message across to a large group of people, what would your message be?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Time & Legacy",
+    question:
+      "If the average human lifespan was 40 years, how would you live your life differently?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Relationships & Connection",
+    question:
+      "What do we all have in common besides our genes that makes us human?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Values & Morality",
+    question:
+      "If you could choose one book as a mandatory read for all high school students, which book would you choose?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question:
+      "Would you rather have less work or more work you actually enjoy doing?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Values & Morality",
+    question: "What is important enough to go to war over?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Growth & Change",
+    question: "Which is worse, failing or never trying?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Self & Identity",
+    question:
+      "When was the last time you listened to the sound of your own breathing?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Self & Identity",
+    question: "What's something you know you do differently than most people?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question: "What does 'The American Dream' mean to you?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question: "Would you rather be a worried genius or a joyful simpleton?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Values & Morality",
+    question:
+      "If you could instill one piece of advice in a newborn baby's mind, what advice would you give?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Relationships & Connection",
+    question: "What is the most desirable trait another person can possess?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question: "What are you most grateful for?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Values & Morality",
+    question: "Is stealing to feed a starving child wrong?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Self & Identity",
+    question: "What do you want most?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Values & Morality",
+    question:
+      "Are you more worried about doing things right, or doing the right things?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Growth & Change",
+    question: "What has life taught you recently?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Growth & Change",
+    question:
+      "What is the one thing you would most like to change about the world?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Self & Identity",
+    question: "Where do you find inspiration?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Self & Identity",
+    question: "Can you describe your life in a six word sentence?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Growth & Change",
+    question:
+      "If we learn from our mistakes, why are we always so afraid to make a mistake?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Time & Legacy",
+    question: "What impact do you want to leave on the world?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Time & Legacy",
+    question: "What is the most defining moment of your life thus far?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Self & Identity",
+    question: "In the haste of your daily life, what are you not seeing?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question:
+      "If life is so short, why do we do so many things we don't like and like so many things we don't do?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Growth & Change",
+    question: "What lifts your spirits when life gets you down?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Time & Legacy",
+    question: "Have you ever regretted something you did not say or do?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Self & Identity",
+    question: "Has your greatest fear ever come true?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Relationships & Connection",
+    question: "Why do we think of others the most when they're gone?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Time & Legacy",
+    question: "What is your most beloved childhood memory?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Relationships & Connection",
+    question: "Is it more important to love or be loved?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Values & Morality",
+    question:
+      "If it all came back around to you, would it help you or hurt you?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Time & Legacy",
+    question:
+      "If you had the chance to go back in time and change one thing would you do it?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Time & Legacy",
+    question:
+      "If a doctor gave you five years to live, what would you try to accomplish?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Relationships & Connection",
+    question:
+      "What is the difference between falling in love and being in love?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question: "Who do you think stands between you and happiness?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Self & Identity",
+    question: "What is the difference between innocence and ignorance?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Values & Morality",
+    question: "What is the simplest truth you can express in words?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question: "What gives your life meaning?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question:
+      "Can there be happiness without sadness? Pleasure without pain? Peace without war?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Time & Legacy",
+    question:
+      "What's the one thing you'd like others to remember about you at the end of your life?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question: "Is there such a thing as perfect?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Self & Identity",
+    question:
+      "To what degree have you actually controlled the course your life has taken?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question: "What does it mean to be human?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Relationships & Connection",
+    question:
+      "If you looked into the heart of your enemy, what do you think you would find that is different from what is in your own heart?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Self & Identity",
+    question: "What do you love most about yourself?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Self & Identity",
+    question: "Where would you most like to go and why?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question:
+      "Is it more important to do what you love or to love what you are doing?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Time & Legacy",
+    question: "What do you imagine yourself doing ten years from now?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Relationships & Connection",
+    question:
+      "What small act of kindness were you once shown that you will never forget?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Time & Legacy",
+    question:
+      "What is your happiest childhood memory? What makes it so special?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Self & Identity",
+    question: "Do you own your things or do your things own you?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Time & Legacy",
+    question:
+      "Would you rather lose all of your old memories or never be able to make new ones?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Growth & Change",
+    question:
+      "How do you deal with someone in a position of power who wants you to fail?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Self & Identity",
+    question: "What do you have that you cannot live without?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Self & Identity",
+    question: "When you close your eyes what do you see?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question: "What sustains you on a daily basis?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Values & Morality",
+    question: "What are your top five personal values?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Relationships & Connection",
+    question: "Why must you love someone enough to let them go?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Self & Identity",
+    question: "Do you ever celebrate the green lights?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Self & Identity",
+    question: "What personal prisons have you built out of fears?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Growth & Change",
+    question: "What one thing have you not done that you really want to do?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Self & Identity",
+    question: "Why are you, you?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Growth & Change",
+    question: "If you haven't achieved it yet what do you have to lose?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Time & Legacy",
+    question:
+      "What three words would you use to describe the last three months of your life?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Values & Morality",
+    question:
+      "Is it ever right to do the wrong thing? Is it ever wrong to do the right thing?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question: "How would you describe 'freedom' in your own words?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Growth & Change",
+    question:
+      "What is the most important thing you could do right now in your personal life?",
+    difficulty: "easy",
+  },
+  {
+    theme: "Values & Morality",
+    question:
+      "If you could ask one person, alive or dead, only one question, who would you ask and what would you ask?",
+    difficulty: "medium",
+  },
+  {
+    theme: "Life Purpose & Meaning",
+    question:
+      "If happiness was the national currency, what kind of work would make you rich?",
+    difficulty: "medium",
+  },
+];
