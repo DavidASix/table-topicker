0125c1c11cf4e949d522603d008cc6029a1699d0
davidasix <david@davidasix.com>
2025-11-09 19:11:35 -0500

    Add an output for the migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,12 @@ import { env } from "~/env";
 export default {
   schema: "./src/server/db/schema/tables/*.ts",
   dialect: "postgresql",
+
+  out: "./src/server/db/migrations/",
+
+  verbose: true,
+  strict: true,
+  
   dbCredentials: {
     url: env.DATABASE_URL,
   },
